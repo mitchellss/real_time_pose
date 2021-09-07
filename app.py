119edd4e619ce9747524f18f722ce345b82783ad
mitchellss <stephen.mitchell2299@gmail.com>
2021-09-06 23:04:04 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,103 @@
+import cv2
+import mediapipe as mp
+import cv2
+import time
+import numpy as np
+
+from pathlib import Path
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.opengl as gl
+import numpy as np
+
+app = QtGui.QApplication([])
+w = gl.GLViewWidget()
+w.show()
+g = gl.GLGridItem()
+w.addItem(g)
+
+b = np.ndarray((33,3))
+
+sp3 = gl.GLScatterPlotItem(pos=b)
+
+w.addItem(sp3)
+
+def update():
+    sp3.setData()
+
+t = QtCore.QTimer()
+t.timeout.connect(update)
+t.start(50)
+
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+
+
+_PRESENCE_THRESHOLD = 0.5
+_VISIBILITY_THRESHOLD = 0.5
+_RGB_CHANNELS = 3
+WHITE_COLOR = (224, 224, 224)
+BLACK_COLOR = (0, 0, 0)
+RED_COLOR = (0, 0, 255)
+GREEN_COLOR = (0, 128, 0)
+BLUE_COLOR = (255, 0, 0)
+
+# For webcam input:
+cap = cv2.VideoCapture(0)
+
+def _normalize_color(color):
+  return tuple(v / 255. for v in color)
+
+with mp_pose.Pose(
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5,
+    model_complexity=2) as pose:
+
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
+
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = pose.process(image)
+
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(
+        image,
+        results.pose_landmarks,
+        mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+
+    a = results.pose_world_landmarks
+    if (a != None):
+        x = 0
+        for landmark in a.landmark:
+            b[x][0] = landmark.x
+            b[x][1] = landmark.y
+            b[x][2] = landmark.z
+            x += 1
+
+    
+
+    cv2.imshow('MediaPipe Pose', image)
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+
+
+cap.release()
+
+## Start Qt event loop unless running in interactive mode.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
