28895e6d5e31d7ffb6c7c2f23c230ad0154a7ea3
mitchellss <stephen.mitchell2299@gmail.com>
2022-03-09 17:54:43 -0500

    fix conflict

---
+++ data_logging/zarr_point_logger.py
@@ -0,0 +1,32 @@
+
+from constants.constants import PATH
+from data_logging.logger import Logger
+import time
+import zarr
+import numpy as np
+import pandas as pd
+
+
+
+class ZarrPointLogger(Logger):
+
+    def __init__(self, fname, **kwargs) -> None:
+        super().__init__(fname)
+        self.data = np.zeros((0,133))
+
+    def log(self, data: np.ndarray):
+        newdata = data.ravel() # Make array 1D
+        newdata = np.insert(newdata, 0, [time.time()]) # Insert time into first column
+        if self.logging:
+            self.data = np.vstack([self.data, newdata])
+
+    def new_log(self):
+        if self.logging:
+            self.close()
+        self.current_time = int(time.time())
+        self.data = np.zeros((0,133))
+
+    def stop_logging(self) -> None:
+        df = pd.DataFrame(self.data)
+        df.to_csv(PATH / "data" / self.folder_name / f"{self.current_time}_{self.fname}.csv", header=None, index=None)
+        zarr.save(PATH / "data" / self.folder_name / f"{self.current_time}_{self.fname}.zarr", self.data)
